a24ed46a838af8c739a9165657182072dd537fd6
Eduardo Freitas <eduardoflorencio96@gmail.com>
2020-11-04 16:05:48 -0300

    A independência do teste

---
+++ test/com/udemy/junit/negocio/GerenciadoraClientesTest_Ex8.java
@@ -0,0 +1,110 @@
+package com.udemy.junit.negocio;
+
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Classe de teste criada para garantir o funcionamento das principais operações
+ * sobre clientes, realidades pela classe {@link GerenciadoraClientes}.
+ * 
+ * @author Eduardo Freitas
+ * @date 04/11/2020
+ */
+public class GerenciadoraClientesTest_Ex8 {
+
+	private GerenciadoraClientes gerClientes;
+	private int idCliente01 = 1;
+	private int idCliente02 = 2;
+	
+	/**
+	 * Método que monta o cenário de teste antes que qualquer método seja executado
+	 * 
+	 * @author Eduardo Freitas
+	 * @date 04/11/2020
+	 */
+	@Before
+	public void setUp() {
+		
+		/* =========== Montagem do cenário =========== */
+		
+		// criando alguns clientes
+		
+		Cliente cliente1 = new Cliente(idCliente01, "Eduardo Freitas", 24, "eduardo@gmail.com", 1, true);
+		Cliente cliente2 = new Cliente(idCliente02, "John Doe", 40, "john@gmail.com", 1, true);
+		
+		// inserindo os clientes criados na lista de clientes do banco
+		List<Cliente> clientesDoBanco = new ArrayList<>();
+		clientesDoBanco.add(cliente1);
+		clientesDoBanco.add(cliente2);
+		
+		gerClientes = new GerenciadoraClientes(clientesDoBanco);
+		
+		// a) Abriu conexão com o BD? Então...
+		// b) Criou arquivos e pastas aqui? Então...
+		// c) Inseriu clientes ficticios na base de dados especificamente para os testes desta classe? Então...
+		
+//		System.out.println("Before foi executado");
+	}
+	
+	/**
+	 * Método que será executado após a execução de um teste
+	 * 
+	 * @author Eduardo Freitas
+	 * @date 04/11/2020
+	 */
+	@After
+	public void tearDown() {
+		gerClientes.limpa();
+		
+		// a) Fecha aqui!!!
+		// b) Apaga todos eles aqui!!!
+		// c) Apaga eles aqui!!!
+		
+//		System.out.println("After foi executado");
+	}
+	
+	/**
+	 * Teste básico da pesquisa de um cliente a partir do seu ID
+	 * 
+	 * @author Eduardo Freitas
+	 * @date 04/11/2020
+	 */
+	@Test
+	public void testPesquisaCliente() {
+		
+		/* =========== Execução =========== */
+		Cliente cliente = gerClientes.pesquisaCliente(idCliente01);
+		
+		/* =========== Verificações =========== */
+		assertThat(cliente.getId(), is(idCliente01));
+		
+	}
+	
+	/**
+	 * Teste básico da remoção de um cliente a partir do seu ID.
+	 * 
+	 * @author Eduardo Freitas
+	 * @date 04/11/2020
+	 */
+	@Test
+	public void testRemoveCliente() {
+		
+		/* =========== Execução =========== */
+		boolean clienteRemovido = gerClientes.removeCliente(idCliente02);
+		
+		/* =========== Verificações =========== */
+		assertThat(clienteRemovido, is(true));
+		assertThat(gerClientes.getClientesDoBanco().size(), is(1));
+		assertNull(gerClientes.pesquisaCliente(idCliente02));
+		
+	}
+	
+}
